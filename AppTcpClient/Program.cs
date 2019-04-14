a3f35c3f0a273c7eeb772bf7afde996dcf2c5c2d
Deberson <deberson.paula@xpi.com.br>
2019-04-14 18:28:44 -0300

    initial release

---
+++ AppTcpClient/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace AppTcpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Connect("127.0.0.1", "Hello there from conn " + i);
+            }
+            
+        }
+
+        static void Connect(string server, string message)
+        {
+            try
+            {
+                var t1 = DateTime.Now;
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer 
+                // connected to the same address as specified by the server, port
+                // combination.
+                int port = 8000;
+                TcpClient client = new TcpClient(server, port);
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+                // Receive the TcpServer.response.
+
+                // Buffer to store the response bytes.
+                data = new byte[256];
+
+                // String to store the response ASCII representation.
+                String responseData = String.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+
+                var t2 = DateTime.Now;
+
+                var t = t2 - t1;
+                var ms = decimal.Divide(t.Ticks, 10000);
+
+                Console.WriteLine("Received: {0} - {1}ms", responseData, ms.ToString("0.000"));
+                // Close everything.
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+            //Console.WriteLine("\n Press Enter to continue...");
+            //Console.Read();
+        }
+    }
+}
